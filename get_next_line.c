864030ecfd349f2d893d8966d05e5c6e8a8d4c90
Geraldo Grapefruit <ggrapefr@pl-b1.msk.21-school.ru>
2021-05-15 18:29:43 +0300

    15.05

---
+++ get_next_line.c
@@ -0,0 +1,18 @@
+#include <get_next_line.h>
+#include <fcntl.h>
+
+int	get_next_line(int fd, char **line)
+{
+	return (0);
+}
+
+int	main(void)
+{
+	int		i;
+	char	*line;
+	int		fd;
+
+	while (get_next_line(4, &line))
+	fd = open("text.txt", O_RDONLY);
+
+}
