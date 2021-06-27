f260c98eadc513b05a9896e51f6c1ac10a5e0ca7
alinakuzmina <alinakuzmina.cooperation@gmail.com>
2021-06-27 18:58:41 +0300

    new

--- get_next_line.c
+++ get_next_line.c
@@ -5,14 +5,20 @@
 
 int	get_next_line(int fd, char **line)
 {
-	char	buf[11];
-	int		byte_was_read;
-	char	*p_n;
-	int		flag;
+	char		buf[11];
+	int			byte_was_read;
+	char		*p_n;
+	int			flag;
+	static char	*reaminder;
 
+	if (fd < 0 || line == NULL ||  BUFFER_SIZE <= 0)
+		return (-1);
 	flag = 1;
+	if (reaminder)
+		*line = ft_strdup(reaminder)
+	else
+		*line = ft_bzero(2);
 	// *line = ft_strnew(1);//аллоцирует память под то количество, которое мы просим и нуль терминат, и заполняет её нулями
-	*line = ft_bzero(2);
 	while (flag && (byte_was_read = read(fd, buf, 10)))
 	{
 		buf[byte_was_read] = '\0';
@@ -20,6 +26,8 @@ int	get_next_line(int fd, char **line)
 		{
 			*p_n = '\0';
 			flag = 0;
+			p_n++;
+			reaminder = ft_strdup(p_n);
 		}
 		*line = ft_strjoin(*line, buf);//склеивает лайн и буфер
 	}
@@ -32,8 +40,14 @@ int	main(void)
 	char	*line;
 	int		fd;
 
-	// while (get_next_line(4, &line))
-	fd = open("text.txt", O_RDONLY);
-	get_next_line(fd, &line);
-	printf("%s\n", line);
+	fd = open("text.txt", O_RDONLY) == -1;
+	while ((i = get_next_line(fd, &line)))
+	{
+		printf ("i = %d %s\n", i, line);
+		free (line);
+	}
+	printf ("i = %d %s\n", i, line);
+	close(fd);
+	free(line);
+	return (0);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -39,3 +39,33 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *)&s[i]);
 	return (NULL);
 }
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((char *)s)[i] = 0;
+		i++;
+	}
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s;
+	int		i;
+
+	i = 0;
+	s = (char *) malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (!s)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		s[i] = s1[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
