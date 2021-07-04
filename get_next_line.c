c4aa9788f0e874cc47084b57b217115c8a3d6df9
Geraldo Grapefruit <ggrapefr@pl-f1.msk.21-school.ru>
2021-07-04 20:09:07 +0300

    yyse

--- get_next_line.c
+++ get_next_line.c
@@ -1,45 +1,34 @@
 #include "get_next_line.h"
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10000000
-#endif
 
-// static	void	ft_pos_n(char **line, char *remainder)
-// {
-// 	char	*tmp;
-// 	char	*p_n;
+// #ifndef BUFFER_SIZE
+// # define BUFFER_SIZE 100
+// #endif
 
-// 	p_n = ft_strchr(remainder, '\n');
-// 	if (p_n)
-// 	{
-// 		*p_n++ = '\0';
-// 		*line = ft_strdup(remainder);
-// 		tmp = remainder;
-// 		remainder = ft_strdup(p_n);
-// 		free(tmp);
-// 		return (1);
-// 	}
-// }
-// static int	ft_byte_was_read(char **line, char *remainder)
-// {
-// 	*line = ft_strdup(remainder);
-// 	free(remainder);
-// 	remainder = NULL;
-// 	return (0);
-// }
+static int	ft_rewrite_remainder(char **line, char **remainder,
+								int byte_was_read)
+{
+	char	*tmp;
+	char	*p_n;
 
-// static void	check_remainder(char **remainder, char *buf)
-// {
-// 	char	*tmp;
-
-// 	if (!remainder)
-// 		*remainder = ft_strdup(buf);
-// 	else
-// 	{
-// 		tmp = *remainder;
-// 		*remainder = ft_strjoin(*remainder, buf);
-// 		free(tmp);
-// 	}
-// }
+	p_n = ft_strchr(*remainder, '\n');
+	if (p_n)
+	{
+		*p_n++ = '\0';
+		*line = ft_strdup(*remainder);
+		tmp = *remainder;
+		*remainder = ft_strdup(p_n);
+		free(tmp);
+		return (1);
+	}
+	if (byte_was_read == 0)
+	{
+		*line = ft_strdup(*remainder);
+		free(*remainder);
+		*remainder = NULL;
+		return (0);
+	}
+	return (-1);
+}
 
 static char	*check_remainder(const char *buf, char *remainder)
 {
@@ -60,9 +49,8 @@ int	get_next_line(int fd, char **line)
 {
 	char		buf[BUFFER_SIZE + 1];
 	static char	*remainder;
-	char		*p_n;
 	int			byte_was_read;
-	char		*tmp;
+	int			returned;
 
 	if (read(fd, NULL, 0) == -1 || BUFFER_SIZE < 1)
 		return (-1);
@@ -73,24 +61,11 @@ int	get_next_line(int fd, char **line)
 		if (byte_was_read == -1)
 			return (-1);
 		remainder = check_remainder(buf, remainder);
-		p_n = ft_strchr(remainder, '\n');
-		if (p_n)
-		{
-			*p_n++ = '\0';
-			*line = ft_strdup(remainder);
-			tmp = remainder;
-			remainder = ft_strdup(p_n);
-			free(tmp);
-			return (1);
-		}
-		if (byte_was_read == 0)
-		{
-			*line = ft_strdup(remainder);
-			free(remainder);
-			remainder = NULL;
-			return (0);
-		}
+		returned = ft_rewrite_remainder(line, &remainder, byte_was_read);
+		if (returned == 0 || returned == 1)
+			return (returned);
 	}
+	return (0);
 }
 
 // int	main(void)
